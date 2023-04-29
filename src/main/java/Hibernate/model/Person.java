99efe8fdd1f0b9b450a82aa0b4a8ccf86e48dcac
BaronVice <toni.sas.228@mail.ru>
2023-04-29 19:03:31 +0800

    More of TextStuff + hibernate

---
+++ src/main/java/Hibernate/model/Person.java
@@ -0,0 +1,26 @@
+package Hibernate.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Person")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+}
